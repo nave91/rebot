08c81317a7c8bdfdb0ab0057af5396ed93b25035
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-09 18:37:15 +0000

    actual json format

--- scripts/convert_sample_posts_to_json.py
+++ scripts/convert_sample_posts_to_json.py
@@ -1,16 +1,20 @@
-import json
 from xml.etree.cElementTree import iterparse
+import json
 
 # get an iterable and turn it into an iterator
-context = iter(iterparse("../sample/stackexchange-posts-sample-big.xml", events=("start", "end")))
+context = iter(iterparse("./sample.xml", events=("start", "end")))
 
 # get the root element
 event, root = next(context)
 assert event == "start"
 
+out = '{ '
+
 for event, elem in context:
     if event == "end" and elem.tag == "row":
-        print json.dumps(elem.items())
+        d = dict(elem.items())
+        out += d['Id'] + ': ' + str(d) + ',' 
         # ... process book elements ...
         root.clear()
-        
+
+print out[:-1] + '}'
