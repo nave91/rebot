0d90d36dc2e20b34b1c0b9de9dc4081d27bb699a
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-09 22:41:26 +0000

    working csv

--- scripts/convert_sample_posts_to_csv.py
+++ scripts/convert_sample_posts_to_csv.py
@@ -9,14 +9,27 @@ context = iter(iterparse("../sample/stackexchange-posts-sample-big.xml", events=
 event, root = next(context)
 assert event == "start"
 rows = []
-header = ['Id', 'PostTypeId', 'Score', 'Body']
+header = ['Id', 'PostTypeId', 'Score']
+answer = 'AcceptedAnswerId'
 for event, elem in context:
     row = ""
     if event == "end" and elem.tag == "row":
         d = dict(elem.items())
         for h in header:
-            row += "\""+str(d[h]).replace('(\n|\")',' ') +"\""+", "
+            # item = d[h].encode('utf-8')
+            # item = item.replace('(?>\P{M}\p{M}*)+','')
+            # item = item.replace('\n', ' ')
+            # item = item.replace('\"', ' ')
+            # item = item.replace('\t', ' ')
+            # item = item.replace(' +',' ')
+            # row += "\""+item +"\""+", "
+            row += str(d[h]) + ','
+        if answer in d.keys():
+            row += str(d[answer]) + ','
+        else:
+            row += 'NULL' + ','
         root.clear()
-    rows.append(row)
+        rows.append(row[:-1])
+header.append(answer)
 print ','.join(header)
 for r in rows: print r
