a7f896f931fff1162ccdd5b230293090c3b6d307
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-11 04:24:04 +0000

    sparkkk to elastic

---
+++ scripts/test_spark_to_es.py
@@ -0,0 +1,25 @@
+# es_spark_test.py
+from pyspark import SparkContext, SparkConf
+
+
+conf = SparkConf().setAppName("ESTest")
+sc = SparkContext(conf=conf)
+
+es = "ec2-52-8-185-215.us-west-1.compute.amazonaws.com:9200"
+
+es_write_conf = {
+    "es.nodes" : es,
+    "es.resource" : "titanic/fail"
+} 
+some_data=sc.parallelize([1,2,3,4])
+some_data_mapped_to_keys=res.map(lambda x: ('key', {'name': str(x), 'sim':0.22}))
+some_data_mapped_to_keys.collect()
+
+
+some_data_mapped_to_keys.saveAsNewAPIHadoopFile(
+    path='-', 
+    outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
+    keyClass="org.apache.hadoop.io.NullWritable", 
+    valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable", 
+    conf=es_write_conf)
+    
