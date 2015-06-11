1666f2c351d932d61923a7cb4cd9f8d21fb8d017
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-11 05:56:24 +0000

    shooting csv to elastic

---
+++ scripts/spark_to_es.py
@@ -0,0 +1,63 @@
+# es_spark_test.py
+from pyspark import SparkContext, SparkConf
+
+
+conf = SparkConf().setAppName("ESTest")
+sc = SparkContext(conf=conf)
+
+es = "ec2-52-8-185-215.us-west-1.compute.amazonaws.com:9200"
+hdfs = "ec2-52-8-194-49.us-west-1.compute.amazonaws.com:9000"
+es_write_conf = {
+    "es.nodes" : es,
+    "es.resource" : "fuck/hive"
+} 
+
+def mapper(line):
+    words = line.split(',')
+    header = ['id','posttypeid','score','answer']
+    d = {}
+    for i,h in enumerate(header):
+        d[h] = words[i]
+    return ('key', d)
+
+file = sc.textFile("hdfs://"+hdfs+"/raw/sample.csv")
+
+words = file.map(lambda line: mapper(line))
+
+words.collect()
+
+words.saveAsNewAPIHadoopFile(
+    path='-', 
+    outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
+    keyClass="org.apache.hadoop.io.NullWritable", 
+    valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable", 
+    conf=es_write_conf)
+
+    
+        
+#$SPARK_HOME/bin/spark-submit --master spark://ip-172-31-2-242:7077 --jars /home/ubuntu/elasticsearch-hadoop-2.1.0.Beta2.jar /home/ubuntu/git/rebot/scripts/spark_to_es.py
+
+# DELETE /stack_data/
+
+# GET /fuck/_search
+# {
+#    "query": {
+#       "match_all": {}
+#    }
+# }
+
+# POST /stack_data/
+# POST /stack_data/fail
+# {
+#     "id": "-1",
+#     "posttypeid": "-2",
+#     "score": "-1",
+#     "answer": "-1"
+# }
+
+# GET /_cat/indices/
+
+# PUT /stack_data/_settings
+# {
+#     "index.indexing.slowlog.threshold.index.info": "1ms" 
+# }
