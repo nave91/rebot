f0924a9592194c6b06490bbd107f7ce884dc9824
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-17 02:55:23 +0000

    adding search and store logs

---
+++ scripts/search_logs.py
@@ -0,0 +1,14 @@
+from pyes import *
+import sys
+
+conn = ES('ec2-52-8-185-215.us-west-1.compute.amazonaws.com:9200')
+q = QueryStringQuery("snippets:INFO")
+results = conn.search(query=q)
+
+for r in results:
+    sys.stderr.write('.')
+    if '[INFO]' in r['snippets']:
+        with open('logs.json','a') as f:
+            f.write(repr(r))
+            
+
