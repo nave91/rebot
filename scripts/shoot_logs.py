d31d1d44f7ecb98619f8fb2158caf01603a2f513
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-17 05:11:57 +0000

    shoot random logs

--- scripts/shoot_logs.py
+++ scripts/shoot_logs.py
@@ -1,9 +1,18 @@
 import sys
+import random
+import ast
+import time
+
 def get_lines(f):
     with open(f, 'r') as f:
         lines = f.readlines()
     return lines
 
 lines = get_lines('logs.json')
-
-print len(lines)
+while 1:
+    i = random.randint(0,len(lines)-1)
+    line = ast.literal_eval(lines[i])
+    out = line['snippets']
+    if '[ERROR]' not in out:
+        time.sleep(1)
+        print out[7:-8]
