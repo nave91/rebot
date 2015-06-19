b6e88ad442386c625ee67eafecd67f43ad71a4c9
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-19 18:26:33 +0000

    getting rebot sample

--- scripts/shoot_logs.py
+++ scripts/shoot_logs.py
@@ -2,42 +2,84 @@ import sys
 import random
 import ast
 import time
-from pyes import *
-import re, string
-    
+import re
+import argparse
+
+args = {
+    'v': 1,
+    'one': False,
+    'sample': True,
+} 
 def get_lines(f):
+    # read all lines and shoot them out
     with open(f, 'r') as f:
         lines = f.readlines()
     return lines
 
-def call_es(error):
-    error = error[:10]
-    conn = ES('ec2-52-8-185-215.us-west-1.compute.amazonaws.com:9200')
-    print error
-    error = re.sub('[\W_]+', '', error)
-    q = QueryStringQuery("ques.snippets:{}".format(error))
-    results = conn.search(query=q)
-    if len(results) > 0:
-        print type(results), len(results)
-        for r in results:
-            print r[0]
 
-def call_rebot():
-    time.sleep(1)
-    error = out[7:-8]
-    #sys.stderr.write(error)
-    #print error
-    print "."
-    call_es(error)
+def print_log(out, sec):
+    time.sleep(sec)
+    error = out[6:-8]
+
+    # Print error to user
+    if args['v'] > 0:
+        sys.stderr.write(error)
+    
+    # Strip error of new lines to make rebot
+    # a bit efficient
+    error = re.sub('\n',' ', error)
+    
+    # Print error to stdout for sweet rebot
+    print error
     
 if __name__ == '__main__':
-    lines = get_lines('logs.json')
-    _i =0 
-    while _i<10:
-        i = random.randint(0,len(lines)-1)
-        line = ast.literal_eval(lines[i])
-        out = line['snippets']
-        if '[ERROR]' not in out:
-            call_rebot()
-        print _i
-        _i += 1
+    
+    desc = "You are looking at Rebot helper CLI interface.\n"+\
+           "This script keeps shooting logs till eternity."
+
+    # Define arguments
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-o","--one",
+                        help="shoot just one log",
+                        action="store_true")
+    parser.add_argument("-s","--sample",
+                        help="keep shooting logs",
+                        action="store_true")
+    parser.add_argument("-v","--verbose",type=int,
+                        help="pump up them verbose jargon")
+    
+    # Parse arguments
+    a = parser.parse_args()
+    
+    # Perform actions
+    if a.one:
+        sys.stderr.write(" [INFO] Just shooting one log out.\n") 
+        args['one'] = True
+    
+    if a.sample:
+        sys.stderr.write(" [INFO] Going to shoot logs forever.\n") 
+        args['sample'] = True
+
+    if a.verbose > -1:
+        args['verbose'] = a.verbose
+
+    if args['one']:
+        pass
+        # get some awesome stuff
+        sys.exit()
+
+    if args['sample']:
+        # keep shooting sample lines
+        lines = get_lines('logs.json')
+        _i = 0 
+        while 1:
+            i = random.randint(0,len(lines)-1)
+            line = ast.literal_eval(lines[i])
+            out = line['snippets']
+            if '[ERROR]' not in out:
+                print_log(out, 2)
+            if args['v'] > 2:
+                sys.stderr.write("#"*20)
+                sys.stderr.write("#"*10+' log '+str(i)+"#"*10)
+                sys.stderr.write("#"*20)
+            _i += 1
