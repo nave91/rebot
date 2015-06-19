6218fb57d963baa0c516af5ce93f120d4c75dff6
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-19 19:13:25 +0000

    wowowoowwo

--- scripts/shoot_logs.py
+++ scripts/shoot_logs.py
@@ -23,7 +23,7 @@ def print_log(out, sec):
 
     # Print error to user
     if args['v'] > 0:
-        sys.stderr.write(error)
+        sys.stderr.write(error+'\n')
     
     # Strip error of new lines to make rebot
     # a bit efficient
@@ -40,10 +40,10 @@ if __name__ == '__main__':
     # Define arguments
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("-o","--one",
-                        help="shoot just one log",
+                        help="shoot just one log to stdin",
                         action="store_true")
     parser.add_argument("-s","--sample",
-                        help="keep shooting logs",
+                        help="keep shooting logs to stdin",
                         action="store_true")
     parser.add_argument("-v","--verbose",type=int,
                         help="pump up them verbose jargon")
@@ -53,23 +53,26 @@ if __name__ == '__main__':
     
     # Perform actions
     if a.one:
-        sys.stderr.write(" [INFO] Just shooting one log out.\n") 
+        sys.stderr.write("### Just shooting one log out.\n\n\n") 
         args['one'] = True
     
     if a.sample:
-        sys.stderr.write(" [INFO] Going to shoot logs forever.\n") 
+        sys.stderr.write("### Going to shoot logs forever.\n\n\n") 
         args['sample'] = True
 
     if a.verbose > -1:
         args['verbose'] = a.verbose
 
     if args['one']:
-        pass
-        # get some awesome stuff
+        # just shoot the one log
+        lines = get_lines('onelog.json')
+        line = ast.literal_eval(lines[0])
+        out = line['snippets']
+        print_log(out,2)
         sys.exit()
 
     if args['sample']:
-        # keep shooting sample lines
+        # randomly keep shooting sample logs
         lines = get_lines('logs.json')
         _i = 0 
         while 1:
