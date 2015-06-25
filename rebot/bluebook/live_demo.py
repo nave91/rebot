193dc5248a65b8193cc9865ed62e891ef8155a0e
nave91 <nalekkalalpudi@mix.wvu.edu>
2015-06-25 08:44:17 +0000

    should be working

--- rebot/bluebook/live_demo.py
+++ rebot/bluebook/live_demo.py
@@ -1,26 +1,37 @@
+import requests
+
 from flask import Blueprint, render_template, request
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 
+def call_server(error):
+    server = 'http://localhost:5000/api/v1/error'
+    params = {
+        'error_log': error
+    }
+    result = {}
+    r = requests.post(server, params)
+    if r.status_code == 200:
+        result['answer_link'] = r.json()['answer_link']
+        result['num_results'] = r.json()['num_results']
+    return result
+
+class SearchForm(Form):
+    logs = TextAreaField('logs', validators=[DataRequired()])
 
 demo_page = Blueprint('demo_page', __name__, template_folder='templates')
 
 @demo_page.route("/demo")  
 def live_demo(): 
     return render_template('demo.html')
-    
-    
-
-class SearchForm(Form):
-    logs = StringField('logs', validators=[DataRequired()])
-
 
 @demo_page.route("/search", methods=['GET', 'POST'])  
 def search(): 
     form = SearchForm()
     if request.method == 'POST' and form.validate():
         import ipdb; ipdb.set_trace()
-        return 'some'
-    
+        result = call_server(form.logs.data)
+        result = str(result)
+        return render_template('search.html', form=form, result=result)
     return render_template('search.html', form=form)
