653dee2a72bb4a14d9aed2619212d2b4c8de0226
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-26 08:15:35 +0000

    adding some changes

--- rebot/bluebook/conf.py
+++ rebot/bluebook/conf.py
@@ -1,17 +1,18 @@
 HDFS_FQDN = "ec2-52-8-214-93.us-west-1.compute.amazonaws.com:9000"
 ES_FQDN = "ec2-52-8-185-215.us-west-1.compute.amazonaws.com:9200"
 
-ES_INDEX = "rebot"
-ES_TYPE = "skynet"
+# ES_INDEX = "rebot"
+ES_INDEX = "something"
+SO_ES_TYPE = "stackoverflow_accepted"
 
 STACKEXCHANGE_XML_FOLDER_NAME = "input/"
-STACKEXCHANGE_XML_FILE_NAME = "Posts.xml" 
-# STACKEXCHANGE_XML_FILE_NAME = "stackexchange-posts-sample.xml" 
+# STACKEXCHANGE_XML_FILE_NAME = "Posts.xml" 
+STACKEXCHANGE_XML_FILE_NAME = "stackexchange-posts-sample.xml" 
 
-STACKEXCHANGE_JSON_QUES_FOLDER_NAME = "jsons/ques/"
-STACKEXCHANGE_JSON_ANS_FOLDER_NAME = "jsons/ans/"
-# STACKEXCHANGE_JSON_QUES_FOLDER_NAME = "jsons/sample-ques/"
-# STACKEXCHANGE_JSON_ANS_FOLDER_NAME = "jsons/sample-ans/"
+# STACKEXCHANGE_JSON_QUES_FOLDER_NAME = "jsons/stackoverflow/ques/"
+# STACKEXCHANGE_JSON_ANS_FOLDER_NAME = "jsons/stackoverflow/ans/"
+STACKEXCHANGE_JSON_QUES_FOLDER_NAME = "jsons/sample-ques/"
+STACKEXCHANGE_JSON_ANS_FOLDER_NAME = "jsons/sample-ans/"
 
 
 STACKEXCHANGE_HEADER_MAP = {
@@ -20,6 +21,7 @@ STACKEXCHANGE_HEADER_MAP = {
     'score': 'Score',
     'answer': 'AcceptedAnswerId',
     'body': 'Body',
-    'snippets': 'snippets'
+    'snippets': 'snippets',
+    'sourcetype': 'sourcetype'
 }
 
